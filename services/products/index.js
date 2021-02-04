cb6ee596a0aeec654735e99045b54d5fbe8baa3a
Niels Peter <nielspeter@npstrandberg.com>
2021-02-04 10:59:01 +0100

    removed federated

--- services/products/index.js
+++ services/products/index.js
@@ -1,5 +1,4 @@
 const { ApolloServer, gql } = require("apollo-server");
-const { buildFederatedSchema } = require("@apollo/federation");
 
 const typeDefs = gql`
   type Query {
@@ -16,42 +15,39 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    Product(_, args) {
-      return products.find(obj => { return obj.upc === args.upc });
+    Product(root, args, context) {
+      return context.products.find(obj => { return obj.upc === args.upc });
     }
   }
 };
 
 const server = new ApolloServer({
-  schema: buildFederatedSchema([
-    {
-      typeDefs,
-      resolvers
-    }
-  ])
+  typeDefs,
+  resolvers,
+  context: {
+    products: [
+      {
+        upc: "1",
+        name: "Table",
+        price: 899,
+        weight: 100
+      },
+      {
+        upc: "2",
+        name: "Couch",
+        price: 1299,
+        weight: 1000
+      },
+      {
+        upc: "3",
+        name: "Chair",
+        price: 54,
+        weight: 50
+      }
+    ]
+  }
 });
 
 server.listen({ port: 4001 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
-
-const products = [
-  {
-    upc: "1",
-    name: "Table",
-    price: 899,
-    weight: 100
-  },
-  {
-    upc: "2",
-    name: "Couch",
-    price: 1299,
-    weight: 1000
-  },
-  {
-    upc: "3",
-    name: "Chair",
-    price: 54,
-    weight: 50
-  }
-];
--- services/products2/index.js
+++ services/products2/index.js
@@ -1,61 +1,53 @@
 const { ApolloServer, gql } = require("apollo-server");
-const { buildFederatedSchema } = require("@apollo/federation");
 
 const typeDefs = gql`
-  extend type Query {
-    topProducts(first: Int = 5): [Product]
-  }
+    type Query {
+        Product(upc: String): Product
+    }
 
-  type Query {
-      Product(upc: String): Product
-  }
-
-  type Product {
-    upc: String!
-    name: String
-    price: Int
-    weight: Int
-  }
+    type Product {
+        upc: String!
+        name: String
+        price: Int
+        weight: Int
+    }
 `;
 
 const resolvers = {
   Query: {
-    Product(_, args) {
-      return products.find(obj => { return obj.upc === args.upc });
+    Product(root, args, context) {
+      return context.products.find(obj => { return obj.upc === args.upc });
     }
   }
 };
 
 const server = new ApolloServer({
-  schema: buildFederatedSchema([
-    {
-      typeDefs,
-      resolvers
-    }
-  ])
+  typeDefs,
+  resolvers,
+  context: {
+    products: [
+      {
+        upc: "1",
+        name: "Table2",
+        price: 899,
+        weight: 100
+      },
+      {
+        upc: "2",
+        name: "Couch2",
+        price: 1299,
+        weight: 1000
+      },
+      {
+        upc: "3",
+        name: "Chair2",
+        price: 54,
+        weight: 50
+      }
+    ]
+  }
 });
 
 server.listen({ port: 4002 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
-
-const products = [
-  {
-    upc: "1",
-    name: "Table2",
-    price: 899,
-    weight: 100
-  },
-  {
-    upc: "2",
-    name: "Couch2",
-    price: 1299,
-    weight: 1000
-  },
-  {
-    upc: "3",
-    name: "Chair2",
-    price: 54,
-    weight: 50
-  }
-];
